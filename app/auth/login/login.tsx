d24c96721928e6c3b90b337654cbf80ba6a87bc5
MEET SIDHDHAPURA <meetgajjar9976@gmail.com>
2024-09-10 01:42:20 +0530

    feat: add login and ui setup

---
+++ app/auth/login/login.tsx
@@ -0,0 +1,15 @@
+import LoginComponent from "@/components/loginComponent/loginComponent";
+import Head from "next/head";
+
+const Login = () => {
+  return (
+    <>
+      <Head>
+        <title>Login | RealEstate</title>
+      </Head>
+      <LoginComponent />
+    </>
+  );
+};
+
+export default Login;
